f5e3fe609244e509d810110d9e067d154d80c06b
ZhengJunChan <zhengjunchan@qq.com>
2017-06-21 16:34:16 +0800

    开发环境自动打开浏览器

---
+++ src/moduls/component/home/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="component_module">
+   首页
+  </div>
+</template>
--- src/router/component/index.js
+++ src/router/component/index.js
@@ -2,6 +2,9 @@ const component = {
     path: '/component',
     component: resolve => require(['@/moduls/component'], resolve),
     children: [{
+        path: '',
+        component: resolve => require(['@/moduls/component/home'], resolve)
+    }, {
         path: 'scroller',
         component: resolve => require(['@/moduls/component/scroller'], resolve)
     }]

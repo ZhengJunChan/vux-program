31c941374cabd560777dc3144d7aeb9fc6647699
ZhengJunChan <zhengjunchan@qq.com>
2017-07-19 19:30:05 +0800

    更新家常菜菜单

--- src/moduls/pc/dinner/home-cooking/data.js
+++ src/moduls/pc/dinner/home-cooking/data.js
@@ -5,8 +5,11 @@ const tabs = [
     	name: '青豆烘丝瓜',
     	link: ''
     }, {
-    	name: '土豆丝',
+    	name: '青椒土豆丝',
     	link: ''
+    }, {
+        name: '酸辣土豆丝',
+        link: ''
     }, {
     	name: '西芹豆干',
     	link: 'http://home.meishichina.com/recipe-285287.html'
@@ -22,12 +25,33 @@ const tabs = [
     }, {
         name: '番茄炒蛋',
         link: ''
+    }, {
+        name: '丝瓜炒蛋',
+        link: ''
+    }, {
+        name: '香肠焖饭',
+        link: ''
+    }, {
+        name: '青豆烘肉',
+        link: ''
+    }, {
+        name: '白菜炒腊肉',
+        link: ''
+    }, {
+        name: '干煸四季豆',
+        link: ''
+    }, {
+        name: '蛋炒饭',
+        link: ''
     }]
   }, {
     name: "大菜",
     dishes: [{
     	name: '糖醋排骨',
     	link: 'http://home.meishichina.com/recipe-9227.html'
+    }, {
+        name: '雪豆炖猪蹄',
+        link: ''
     }]
   }
 ];

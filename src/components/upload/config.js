8af06284fdc3b217b43caecd7e7748e8319d5493
ZhengJunChan <zhengjunchan@qq.com>
2017-07-22 17:46:41 +0800

    图片上传插件

--- src/components/upload/config.js
+++ src/components/upload/config.js
@@ -1,6 +1,11 @@
 const config = {
     swf: '/static/libs/Uploader.swf', // swf文件路径
     imgApi: '/files/image/upload',  // 图片上传接口
+    img: {
+    	api: '/files/image/upload',
+    	thumbnailWidth: 100, // 缩略图的宽度
+    	thumbnailHeight: 100, // 缩略图的高度
+    },
     musicApi: '/files/music/upload'  // 歌曲上传接口
 };
 
--- src/components/upload/img.vue
+++ src/components/upload/img.vue
@@ -1,15 +1,15 @@
 <!-- [upload_img_component 上传图片插件]   @Author: 郑君婵   @DateTime: 2017-07-19 -->
 <template>
-    <div class="upload_component upload_img_component">
-        <!--用来存放item-->
-        <div>
-            <div v-for="file in fileList" :id="file.id" class="file-item thumbnail">
-                <img :src="file.src" v-if="file.src">
-                <span v-if="!file.src">不能预览</span>
-                <div class="info" v-text="file.name"></div>
-            </div>
+    <div class="upload_component upload_img_component clear_float">
+        <div v-for="(file, index) in fileList" :id="file.id" class="file_item grey_circle_box thumbnail">
+            <img :src="file.src">
+            <span class="delete_btn" @click="remove(index, file.id)">
+                <img src="./images/icon_delete.png">
+            </span>
         </div>
-        <div id="filePicker">选择图片</div>
+        <div id="filePicker" class="upload_btn grey_circle_box" v-show="fileList.length<limitNum">
+            添加图片(0/{{limitNum}})
+        </div>
     </div>
 </template>
 
@@ -18,26 +18,37 @@ import config from './config.js';
 
 export default {
     props: {
+        // 是否自动上传
         auto: {
             type: Boolean,
             default: true
         },
+        // 限制上传的图片个数
         limitNum: {
             type: Number,
-            default: 1
+            default: 3
         }
     },
     data() {
         return {
+            uploader: {},
             fileList: []
+        }
+    },
+    watch: {
+        fileList: function(val) {
+            let textTip = '添加图片(' + val.length + '/' + this.limitNum + ')';
+
+            $('#filePicker').find('.webuploader-pick').text(textTip);
         }
     },
     mounted(){
         let _this = this;
-        let uploader = WebUploader.create({
+
+        this.uploader = WebUploader.create({
             auto: this.auto, // 选完文件后，是否自动上传。
             swf: config.swf, // swf文件路径
-            server: config.imgApi, // 上传接口
+            server: config.img.api, // 上传接口
             fileNumLimit: _this.limitNum, // 限制上传文件个数
 
             // 选择文件的按钮。可选。
@@ -60,32 +71,27 @@ export default {
         });
 
         // 当有文件添加进来的时候
-        uploader.on( 'fileQueued', function( file ) {
-            console.log(file);
-            let thumbnailWidth = 100;
-            let thumbnailHeight = 100;
-
+        this.uploader.on( 'fileQueued', function( file ) {
             // 创建缩略图
             // 如果为非图片文件，可以不用调用此方法。
             // thumbnailWidth x thumbnailHeight 为 100 x 100
-            uploader.makeThumb( file, function( error, src ) {
+            _this.uploader.makeThumb( file, function( error, src ) {
                 if ( error ) {
                     return;
                 }
 
-                file.scr = src;
-            }, thumbnailWidth, thumbnailHeight );
-            console.log(_this.fileList);
-            _this.fileList.push(file);
+                file.src = src;
+            }, config.img.thumbnailWidth, config.img.thumbnailHeight);
         });
 
         // 文件上传成功，给item添加成功class, 用样式标记上传成功。
-        uploader.on( 'uploadSuccess', function( file ) {
-            $( '#'+file.id ).addClass('upload-state-done');
+        this.uploader.on( 'uploadSuccess', function( file ) {
+            _this.fileList.push(file);
+            _this.imgListChange()
         });
 
         // 文件上传失败，显示上传出错。
-        uploader.on( 'uploadError', function( file ) {
+        this.uploader.on( 'uploadError', function( file ) {
             var $li = $( '#'+file.id ),
                 $error = $li.find('div.error');
 
@@ -96,6 +102,17 @@ export default {
 
             $error.text('上传失败');
         });
+    },
+    methods: {
+        remove (index, fileId) {
+            this.fileList.splice(index, 1);
+            this.uploader.removeFile(fileId, true);
+
+            this.imgListChange();
+        },
+        imgListChange () {
+            this.$emit('img-list-change', this.fileList);
+        }
     }
 };
 </script>
